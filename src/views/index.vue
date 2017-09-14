07facb1c6417bdc06c6ca38861a5f37f84ac051f
YangRunkangBla <1743703238@qq.com>
2017-09-14 23:32:27 +0800

    index

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,58 +1,87 @@
 <style scoped>
-    .index {
-        width: 100%;
-        position: absolute;
-        top: 0;
-        bottom: 0;
-        left: 0;
+    .layout{
+        border: 1px solid #d7dde4;
+        background: #f5f7f9;
+    }
+    .layout-logo{
+        width: 100px;
+        height: 30px;
+        background: #5b6270;
+        border-radius: 3px;
+        float: left;
+        position: relative;
+        top: 15px;
+        left: 20px;
+    }
+    .layout-nav{
+        width: 420px;
+        margin: 0 auto;
+    }
+    .layout-assistant{
+        width: 300px;
+        margin: 0 auto;
+        height: inherit;
+    }
+    .layout-breadcrumb{
+        padding: 10px 15px 0;
+    }
+    .layout-content{
+        min-height: 200px;
+        margin: 15px;
+        overflow: hidden;
+        background: #fff;
+        border-radius: 4px;
+    }
+    .layout-content-main{
+        padding: 10px;
+    }
+    .layout-copy{
         text-align: center;
-    }
-
-    .index h1 {
-        height: 150px;
-    }
-
-    .index h1 img {
-        height: 100%;
-    }
-
-    .index h2 {
-        color: #666;
-        margin-bottom: 200px;
-    }
-
-    .index h2 p {
-        margin: 0 0 50px;
-    }
-
-    .index .ivu-row-flex {
-        height: 100%;
+        padding: 10px 0 20px;
+        color: #9ea7b4;
     }
 </style>
 <template>
-    <div class="index">
-        <Row type="flex" justify="center" align="middle">
-            <Col span="24">
-                <h1>
-                    <img src="https://raw.githubusercontent.com/iview/iview/master/assets/logo.png">
-                </h1>
-                <h2>
-                    <p>Welcome to your iView app!</p>
-                    <Button type="ghost" @click="handleStart">Start iView</Button>
-                </h2>
-            </Col>
-        </Row>
+    <div class="layout">
+        <Menu mode="horizontal" theme="dark" active-name="1">
+            <div class="layout-logo"></div>
+            <div class="layout-nav">
+                <MenuItem name="1">
+                    <Icon type="ios-navigate"></Icon>
+                    导航一
+                </MenuItem>
+                <MenuItem name="2">
+                    <Icon type="ios-keypad"></Icon>
+                    导航二
+                </MenuItem>
+                <MenuItem name="3">
+                    <Icon type="ios-analytics"></Icon>
+                    导航三
+                </MenuItem>
+                <MenuItem name="4">
+                    <Icon type="ios-paper"></Icon>
+                    导航四
+                </MenuItem>
+            </div>
+        </Menu>
+        <Menu mode="horizontal" active-name="1">
+            <div class="layout-assistant">
+                <MenuItem name="1">二级导航</MenuItem>
+                <MenuItem name="2">二级导航</MenuItem>
+                <MenuItem name="3">二级导航</MenuItem>
+            </div>
+        </Menu>
+        <div class="layout-breadcrumb">
+            <Breadcrumb>
+                <BreadcrumbItem href="#">首页</BreadcrumbItem>
+                <BreadcrumbItem href="#">应用中心</BreadcrumbItem>
+                <BreadcrumbItem>某应用</BreadcrumbItem>
+            </Breadcrumb>
+        </div>
     </div>
 </template>
 <script>
     export default {
-        methods: {
-            handleStart() {
-                this.$Modal.info({
-                    title: 'Bravo',
-                    content: 'Now, enjoy the convenience of iView.'
-                });
-            }
-        }
-    };
+        
+    }
 </script>
