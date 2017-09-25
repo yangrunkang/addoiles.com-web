7fa38c03197a1d35fbde5c745b5f371aad96fd87
yangrunkang <1743703238@qq.com>
2017-09-25 12:32:54 +0800

    Add: 请求后台数据
    - 解决方法: 在Spring中添加@CrossOrigin注解

--- src/views/index.vue
+++ src/views/index.vue
@@ -151,6 +151,11 @@
                 title: '(｡･∀･)ﾉﾞ嗨！您好！',
                 desc: '在这里，你可以尽情的展现你的内心世界！'
             });
+
+            this.$http.get('http://localhost:8081/addoiles.com/getLatestHots').then(function (res) {
+                console.log(res.data);
+            });
+
         }
 	}
 </script>
