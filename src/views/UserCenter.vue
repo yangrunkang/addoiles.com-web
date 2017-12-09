be4189478a8e19f4949be109c61c9c11fdea7ee7
yangrunkang <1743703238@qq.com>
2017-12-09 16:43:36 +0800

    Add: 新界面 用户中心

---
+++ src/views/UserCenter.vue
@@ -0,0 +1,27 @@
+<style scoped lang="less">
+    @import "../styles/common.css";
+
+</style>
+<template>
+    <div>
+
+    </div>
+</template>
+<script>
+    export default {
+        data () {
+            return {
+
+            }
+        },
+        methods: {
+
+        },
+        computed: {
+
+        },
+        mounted () {
+
+        }
+    }
+</script>
