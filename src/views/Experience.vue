2a1ec6bf659feabab31d1b7a391bb1ce283ff804
yangrunkang <1743703238@qq.com>
2018-01-05 12:25:23 +0800

    Ref: 页面重构;加载逻辑变动

--- src/views/Experience.vue
+++ src/views/Experience.vue
@@ -22,7 +22,7 @@
     /*分享内容*/
     .experience-content {
         background:#eee;
-        padding:20px
+        padding:7px
     }
 
     /*分享按钮*/
@@ -32,6 +32,27 @@
         font-size: xx-large;
         text-align: center;
     }
+
+
+
+    .p-right {
+        text-align: right;
+        color: rgba(91, 106, 97, 0.45);
+        padding-right: 7px;
+        font-size: 14px;
+    }
+
+    .p-left {
+        text-align: left;
+        margin-top: 7px;
+        font-size: 15px;
+    }
+
+    .experience-list {
+        margin-bottom: 10px;
+    }
+
+
 </style>
 <template>
     <div>
@@ -41,55 +62,84 @@
             <Icon type="ios-lightbulb-outline" slot="icon"></Icon>
             <template slot="desc">不虚度人生,让自己的人生少点遗憾</template>
         </Alert>
-        <!--经历-->
-        <Row class="experience-content" :gutter="16" style="margin-bottom: 10px" v-for="(experience,index) in experienceDtoList" :key="experience.id">
-            <i-col span="19">
-                <!--具体的经历-->
-                <Card :bordered="false" style="margin-bottom: 5px">
-                    <p slot="title" class="auto-break-line" style="height: auto;font-size: 18px;">
-                        {{experience.title}}
-                        <Button type="info" shape="circle" style="float: right;" v-show="experience.isShowEditBtn" @click="toEditExperience(experience.id)">编辑</Button>
-                    </p>
-                    <p v-html="experience.content" class="auto-break-line"></p> <!--显示html样式文本-->
-                </Card>
-                <!--评论-->
-                <h2>评论</h2>
-                <Alert type="success" v-for="comment in experience.commentList" :key="comment.id" style="margin: 3px auto;">
-                    <Tag type="border" color="green">{{comment.createTime}}</Tag><Tag color="green">{{comment.userName}}</Tag>
-                    {{comment.content}}
-                </Alert>
-            </i-col>
-            <i-col span="5">
-                <h2>感想+</h2>
-                <Input type="textarea" :rows="3" placeholder="写下你此刻想说的" v-model="commentContent[index]" />
-                <Button type="success" long style="margin-top: 5px" @click="toComment(experience.id,index)">评价</Button><!--experience.id是experienceId-->
-                <Alert style="margin: 3px auto;">
-                    <strong>评分:</strong>
-                    <Rate v-model="experience.rates" @on-change="toRates(experience.id,$event)"></Rate>
-                </Alert>
-                <Tag type="border"  color="blue"><strong>作者:</strong>{{experience.userName}}</Tag>
-                <Tag type="border"  color="green"><strong>发布时间:</strong>{{experience.createTime}}</Tag>
-            </i-col>
-        </Row>
+
+        <div>
+            <Card class="experience-list" v-for="experience in experienceList" :key="experience.id">
+                <h2 class="auto-break-line">
+                    {{experience.title}}
+                </h2>
+                <p class="p-left">
+                    <span  @click="getExperience(experience.id)"><Icon type="paper-airplane"></Icon>&nbsp;查看详情</span>
+                    &nbsp;&nbsp;&nbsp;
+                    <Icon type="calendar"></Icon>&nbsp;{{experience.createTime}}
+                    &nbsp;&nbsp;&nbsp;
+                    <Icon type="ios-speedometer-outline"></Icon>&nbsp;获得评分:{{experience.rates}}
+                    &nbsp;&nbsp;&nbsp;
+                    <Icon type="person"></Icon>&nbsp;{{experience.userName}}
+                </p>
+            </Card>
+        </div>
         <Button type="info" size="large" long style="width: 100%;margin-top: 10px" @click="loadMore()">加载更多</Button>
+
+        <!--具体经历的模态框-->
+        <Modal v-model="showExperienceModal"
+               width="1195px"
+               :styles="{top: '20px'}"
+        >
+            <Row class="experience-content" :gutter="16" style="margin-bottom: 10px">
+                <i-col span="18">
+                    <!--具体的经历-->
+                    <Card :bordered="false" style="margin-bottom: 5px">
+                        <p slot="title" class="auto-break-line" style="height: auto;font-size: 18px;">
+                            {{experienceDto.title}}
+                            <Button type="info" shape="circle" style="float: right;" v-show="experienceDto.isShowEditBtn" @click="toEditExperience(experienceDto.id)">编辑</Button>
+                        </p>
+                        <p v-html="experienceDto.content" class="auto-break-line"></p> <!--显示html样式文本-->
+                    </Card>
+                    <!--评论-->
+                    <h2>评论</h2>
+                    <Alert type="success" v-for="comment in experienceDto.commentList" :key="comment.id" style="margin: 3px auto;">
+                        <Tag type="border" color="green">{{comment.createTime}}</Tag><Tag color="green">{{comment.userName}}</Tag>
+                        {{comment.content}}
+                    </Alert>
+                </i-col>
+                <i-col span="6">
+                    <h2>感想+</h2>
+                    <Input type="textarea" :rows="3" placeholder="写下你此刻想说的" v-model="commentContent" />
+                    <Button type="success" long style="margin-top: 5px" @click="toComment(experienceDto.id)">评价</Button><!--experienceDto.id是experienceId-->
+                    <Alert style="margin: 3px auto;">
+                        <strong>评分:</strong>
+                        <Rate v-model="experienceDto.rates" @on-change="toRates(experienceDto.id,$event)"></Rate>
+                    </Alert>
+                    <Tag type="border"  color="blue"><strong>作者:</strong>{{experienceDto.userName}}</Tag>
+                    <Tag type="border"  color="green"><strong>发布时间:</strong>{{experienceDto.createTime}}</Tag>
+                </i-col>
+            </Row>
+            <div slot="footer">
+                <p style="text-align: center;font-weight: bold">来自油站:不虚度人生,让自己的人生少点遗憾</p>
+            </div>
+        </Modal>
     </div>
 </template>
 <script>
 
     import Vue from 'vue';
-    import Cookies from 'js-cookie';
 
     export default {
         data () {
             return {
+                //是否展示具体经历的模态框
+                showExperienceModal:false,
                 // 经历标题
                 experienceTitle : '',
                 // 经历内容
                 content : '',
                 //评论内容
-                commentContent:[],
-                //经历 + 评论
-                experienceDtoList:[],
+                commentContent:'',
+                //经历列表
+                experienceList:[],
+                //具体的经历
+                experienceDto:{},
                 //列表页是否显示编辑按钮
                 isShowEditBtn : false,
                 //页面查询基础Dto
@@ -117,7 +167,7 @@
                 this.$router.push("/OilEditor");
             },
             /*去评论*/
-            toComment(experienceId,index){
+            toComment(experienceId){
 
                 this.$store.commit('validateLogin',this);
 
@@ -126,7 +176,7 @@
                     return;
                 }
 
-                let commentContent = this.commentContent[index];
+                let commentContent = this.commentContent;
 
                 if(!this.addoileUtil.validateReq(commentContent)){
                     this.$Notice.info({
@@ -153,13 +203,13 @@
                             title: '<h6>评论成功</h6>'
                         });
                         //显示评论信息
-                        this.experienceDtoList[index].commentList.unshift({
+                        this.experienceDto.commentList.unshift({
                             createTime:'刚刚',
                             userName:'我',
                             content:commentContent
                         });
                         //清空数据
-                        this.commentContent[index] = '';
+                        this.commentContent = '';
                     }else{
                         this.$Notice.info({
                             title: '<h6>系统出错了,抱歉</h6>'
@@ -192,55 +242,79 @@
                     //nothing
                 })
             },
-            //获取经历
+            /**
+             * 获取经历列表
+             */
             getExperienceList(){
 
                 this.queryDto.articleType = 0;
 
                 this.axios.post('getExperienceList',this.queryDto).then(function (response) {
                     if(response.data.code == 0){
-                        let experienceDtoList = response.data.data; //List<ExperienceDto>
-                        if(experienceDtoList.length >= 0){
-
-                            for(let i = 0; i < experienceDtoList.length; i++){ //ExperienceDto
-                                let experienceDto = experienceDtoList[i];
-                                //评论
-                                let commentList = experienceDto.commentList;
-                                let _commentList = [];
-                                if(commentList.length > 0){
-                                    for(let j = 0; j < commentList.length; j++){
-                                        let comment = commentList[j];
-                                        _commentList.push(
-                                                {
-                                                    createTime:this.addoileUtil.formatUnixTime(comment.createTime),
-                                                    userName:comment.userName,
-                                                    content:comment.content
-                                                }
-                                        );
-                                    }
-                                }else{
-                                    _commentList.push({
-                                        createTime:"Now",
-                                        userName:"系统提示",
-                                        content:"本文暂无评论,你的机会来了,快在右边写下你的感想吧"});
-                                }
+                        let experienceList = response.data.data; //List<ExperienceDto>
+                        if(experienceList.length > 0){
+                            for(let i = 0; i < experienceList.length; i++){ //ExperienceDto
                                 //经历
-                                let article = experienceDto.article;
-                                //当前用户id
-                                let currentUserId = sessionStorage.getItem("userId");
-                                this.experienceDtoList.push(
-                                        {
-                                            id:article.articleId, //注意这里的id是articleId
-                                            userName:article.userName,
-                                            title:article.title,
-                                            content:article.content,
-                                            rates:article.rates,
-                                            createTime:this.addoileUtil.formatUnixTime(article.createTime),
-                                            commentList:_commentList,
-                                            isShowEditBtn : this.addoileUtil.isCurrentUser(article.userId,currentUserId)
-                                        });
+                                let experience = experienceList[i];
+                                this.experienceList.push({
+                                    //注意这里的id是articleId
+                                    id:experience.articleId,
+                                    userName:experience.userName,
+                                    title:experience.title,
+                                    rates:experience.rates,
+                                    createTime:this.addoileUtil.formatUnixTime(experience.createTime),
+                                });
                             }
+                        }else{
+                            this.$store.commit('loadAll',this);
                         }
+                    }
+                }.bind(this));
+            },
+            //获取具体经历
+            getExperience(experienceId){
+
+                this.queryDto.businessId = experienceId;
+
+                this.axios.post('getExperience',this.queryDto).then(function (response) {
+                    if(response.data.code == 0){
+                        // ExperienceDto
+                        let _experienceDto = response.data.data;
+
+                        //评论
+                        let commentList = _experienceDto.commentList;
+                        let _commentList = [];
+                        if(commentList.length > 0){
+                            for(let j = 0; j < commentList.length; j++){
+                                let comment = commentList[j];
+                                _commentList.push({
+                                    createTime:this.addoileUtil.formatUnixTime(comment.createTime),
+                                    userName:comment.userName,
+                                    content:comment.content
+                                });
+                            }
+                        }else{
+                            _commentList.push({
+                                createTime:"Now",
+                                userName:"系统提示",
+                                content:"本文暂无评论,你的机会来了,快在右边写下你的感想吧"});
+                        }
+                        //经历
+                        let article = _experienceDto.article;
+                        //当前用户id
+                        let currentUserId = sessionStorage.getItem("userId");
+                        this.experienceDto = {
+                            //注意这里的id是articleId
+                            id:article.articleId,
+                            userName:article.userName,
+                            title:article.title,
+                            content:article.content,
+                            rates:article.rates,
+                            createTime:this.addoileUtil.formatUnixTime(article.createTime),
+                            commentList:_commentList,
+                            isShowEditBtn : this.addoileUtil.isCurrentUser(article.userId,currentUserId)
+                        };
+                        this.showExperienceModal = true;
                     }
                 }.bind(this));
             },
