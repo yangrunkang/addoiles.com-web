36fb9ebd070c37f223db1ecd14ed7be87614e952
yangrunkang <1743703238@qq.com>
2018-01-06 18:09:36 +0800

    Add: 图片上传

---
+++ src/views/manager/file-upload.vue
@@ -0,0 +1,32 @@
+<style lang="less">
+    @import "../../styles/common.css";
+    @import 'upload.less';
+</style>
+
+
+<template>
+    <div>
+        <Card>
+            <Upload action="localhost:8081/addoiles.com/addFirstPage">
+                <Button type="ghost" icon="ios-cloud-upload-outline">上传文件</Button>
+            </Upload>
+        </Card>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'file-upload',
+    data () {
+        return {
+            uploadList: []
+        };
+    },
+    methods: {
+
+    },
+    mounted () {
+
+    }
+};
+</script>
